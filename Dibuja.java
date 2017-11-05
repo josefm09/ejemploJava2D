6678d25841fb51a4561ec01c597103cc9455b2a7
José Flores <josecarlosfloresmoran@hotmail.com>
2017-11-05 15:04:45 -0700

    Cambios en dibuja
    Se agregaron muchas cosas

--- Dibuja.java
+++ Dibuja.java
@@ -112,9 +112,15 @@ class MiPanel extends JPanel{
 		 }
                  
                  //Separacion del techo
+                 Color sombra_base = new Color(0, 0, 0, 80);
                  Color separacion = new Color(251, 148, 53);
                  g2d.setPaint(separacion);
                  g2d.fillRect(45, 390, 536, 15);
+                 //Sombre de la separacion
+                 g2d.setPaint(sombra_base);
+                 g2d.fillRect(220, 395, 196, 10);
+                 g2d.setPaint(separacion);
+                 g2d.fillRect(230, 395, 176, 15);
                  
                  //Techo base del edificio 
                  int x[]={30,100,526,596};
@@ -122,6 +128,59 @@ class MiPanel extends JPanel{
                  Color techo = new Color(114, 102, 88);
                  g.setColor(techo);
                  g.fillPolygon(x,y,4);
+                 
+                 //Sombra Techo triangulo
+                 int x1[]={200, 315, 436};
+                 int y1[]={396, 335, 396};
+                 g.setColor(sombra_base);
+                 g.fillPolygon(x1, y1, 3);
+                 
+                 //Torre
+                 Color torre = new Color(241, 100, 31);
+                 g2d.setPaint(torre);
+                 g2d.fillRect(277, 286, 81, 110);
+                 
+                 //Separacion de la torre
+                 g2d.setPaint(separacion);
+                 g2d.fillRect(267, 276, 101, 10);
+                 
+                 //Techo torre
+                 int x3[]={252, 315, 385};
+                 int y3[]={276, 236, 276};
+                 g.setColor(techo);
+                 g.fillPolygon(x3, y3, 3);
+                 
+                 //Base del relog
+                 int xcirc = 316;
+                 int ycirc = 316;
+                 int rcirc = 43;
+                 xcirc = xcirc-(rcirc/2);
+                 ycirc = ycirc-(rcirc/2);
+                 g.setColor(separacion);
+                 g.fillOval(xcirc,ycirc,rcirc,rcirc);
+                 
+                 
+                 //Relog
+                 int xcirc2 = 316;
+                 int ycirc2 = 316;
+                 int rcirc2 = 30;
+                 xcirc2 = xcirc2-(rcirc2/2);
+                 ycirc2 = ycirc2-(rcirc2/2);
+                 g.setColor(Color.WHITE);
+                 g.fillOval(xcirc2,ycirc2,rcirc2,rcirc2);
+                 
+                 //Manecillas
+                 g2d.setPaint(Color.BLACK);
+                 g2d.fillRect(308, 316, 8, 2);
+                 g2d.setPaint(Color.BLACK);
+                 g2d.fillRect(316, 307, 2, 11);
+                 
+                 //Techo triangulo
+                 int x2[]={215, 315, 416};
+                 int y2[]={396, 345, 396};
+                 g.setColor(techo);
+                 g.fillPolygon(x2, y2, 3);
+                 
                  
 		 //Base del edificio 
                  Color base = new Color(185,190,193);
@@ -131,10 +190,44 @@ class MiPanel extends JPanel{
                  g2d.setPaint(base_blanca);
                  g2d.fillRect(40, 550, 546, 5);
                  
+                 
                  //Sombra del cuadro frontal
                  Color sombra_cuadro = new Color(0, 0, 0, 80);
                  g2d.setPaint(sombra_cuadro);
-                 g2d.fillRect(230, 405, 166, 145);
+                 g2d.fillRect(230, 405, 176, 155);
+                 
+                 //Escalones
+                 //Escalon 1
+                 g2d.setPaint(base_blanca);
+                 g2d.fillRect(235, 550, 166, 5);
+                 g2d.setPaint(base);
+                 g2d.fillRect(235, 555, 166, 5);
+                 //Escalon 2
+                 g2d.setPaint(sombra_base);
+                 g2d.fillRect(225, 560, 186, 10);
+                 g2d.setPaint(base_blanca);
+                 g2d.fillRect(230, 560, 176, 5);
+                 g2d.setPaint(base);
+                 g2d.fillRect(230, 565, 176, 5);
+                 //Escalon 3
+                 g2d.setPaint(sombra_base);
+                 g2d.fillRect(220, 570, 196, 10);
+                 g2d.setPaint(base_blanca);
+                 g2d.fillRect(225, 570, 186, 5);
+                 g2d.setPaint(base);
+                 g2d.fillRect(225, 575, 186, 5);
+                 
+                 
+                 //Cuadro frontal
+                 Color cuadro_frontal = new Color(241, 100, 31);
+                 g2d.setPaint(cuadro_frontal);
+                 g2d.fillRect(240, 405, 156, 145);
+                 
+                 //Separacion del primer piso
+                 g2d.setPaint(base_blanca);
+                 g2d.fillRect(234, 465, 168, 5);
+                 g2d.setPaint(base);
+                 g2d.fillRect(234, 470, 168, 5);
 		 
 		 //Banqueta
 		 Color gris_calle = new Color(167,168,170);
